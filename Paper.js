0820ec897edcf5df41cdb3a850120f4b9b484836
EkowWhiteHatJr <83500352+EkowWhiteHatJr@users.noreply.github.com>
2021-08-20 11:23:55 +0000

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,22 @@
+class Paper{
+
+constructor()   {
+
+var peper_options={
+
+    isStatic:false,
+    restitution:0.3,
+    friction:0,
+    density:1.2
+}
+this.image = loadImage(sprites/"paper.png")
+
+
+}
+
+
+
+
+
+
+}
